c80775b4542deceef0ffcde16b90cfad667da9b6
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2017-05-06 22:02:27 -0400

    Added video progress bar to detail shelves

--- js/src/pages/Video.js
+++ js/src/pages/Video.js
@@ -3,6 +3,7 @@ import { prepareUrl, getPath } from '../common/fetch';
 import escape from 'escape-html';
 import play from '../common/play';
 import VideoTile from '../Views/Video';
+import GetAllProgressPromise from '../common/GetAllProgress';
 
 const template = data => `
     <document>
@@ -93,78 +94,77 @@ const Page = ATV.Page.create({
             resolve();
             return;
         }
-        ATV.Ajax.get(prepareUrl(options.detailUrl))
-            .then(response => {
-                const result = ATV._.get(response, 'response.results', {});
-                const data = {
-                    id: result.id,
-                    title: escape(result.name),
-                    subtitle: escape(result.deck),
-                    video: result.low_url,
-                    info: [],
-                    row: [],
-                    length: Math.floor(result.length_seconds),
-                    promises: [] // this gets used later for subsequent calls
-                };
+        return Promise.all([
+            GetAllProgressPromise,
+            ATV.Ajax.get(prepareUrl(options.detailUrl))
+                .then(response => {
+                    const result = ATV._.get(response, 'response.results', {});
+                    const data = {
+                        id: result.id,
+                        title: escape(result.name),
+                        subtitle: escape(result.deck),
+                        video: result.low_url,
+                        info: [],
+                        row: [],
+                        length: Math.floor(result.length_seconds),
+                        promises: [] // this gets used later for subsequent calls
+                    };
 
-                let hours = Math.floor(result.length_seconds / 3600);
-                let minutes = Math.floor(Math.floor(result.length_seconds % 3600) / 60);
-                let seconds = result.length_seconds % 60;
+                    let hours = Math.floor(result.length_seconds / 3600);
+                    let minutes = Math.floor(Math.floor(result.length_seconds % 3600) / 60);
+                    let seconds = result.length_seconds % 60;
 
-                hours = hours < 1 ? '' : `${hours}:`;
-                minutes = `${minutes < 10 ? '0' : ''}${minutes}:`;
-                seconds = `${seconds < 10 ? '0' : ''}${seconds}`;
+                    hours = hours < 1 ? '' : `${hours}:`;
+                    minutes = `${minutes < 10 ? '0' : ''}${minutes}:`;
+                    seconds = `${seconds < 10 ? '0' : ''}${seconds}`;
 
 
-                data.info.push({ name: "Runtime", value: `${hours}${minutes}${seconds}` });
-                data.info.push({ name: "Published", value: result.publish_date.split(' ')[0] });
-                data.info.push({ name: "Type", value: result.video_type });
-                data.info.push({ name: "Posted By", value: result.user });
+                    data.info.push({ name: "Runtime", value: `${hours}${minutes}${seconds}` });
+                    data.info.push({ name: "Published", value: result.publish_date.split(' ')[0] });
+                    data.info.push({ name: "Type", value: result.video_type });
+                    data.info.push({ name: "Posted By", value: result.user });
 
-                if (result.image) {
-                    data.image = result.image.screen_url;
+                    if (result.image) {
+                        data.image = result.image.screen_url;
+                    }
+
+                    if (result.hd_url) {
+                        data.video = result.hd_url;
+                        data.isHD = true;
+                    } else if (result.high_url) {
+                        data.video = result.high_url;
+                    }
+
+                    if (result.video_categories && result.video_categories.length) {
+                        data.categories = result.video_categories.map(({ name, id }) => ({
+                            name: escape(name),
+                            id,
+                            type: 'video_categories'
+                        }));
+                    }
+
+                    if (result.video_shows) {
+                        data.shows = result.video_shows.map(({ name, id }) => ({
+                            name: escape(name),
+                            id,
+                            type: 'video_show'
+                        }));
+                    }
+                    return { data };
+                })
+        ])
+            .then(responses => {
+                const { savedTimes, data } = responses.reduce((prev, curr) => Object.assign({}, prev, curr), {});
+                console.log(data, savedTimes);
+                data.savedTime = ATV._.get(savedTimes, `${data.id}.savedTime`, 0);
+                if (data.savedTime < 0) {
+                    data.savedTime = 0;
+                    data.hideRestart = true;
                 }
-
-                if (result.hd_url) {
-                    data.video = result.hd_url;
-                    data.isHD = true;
-                } else if (result.high_url) {
-                    data.video = result.high_url;
-                }
-
-                if (result.video_categories && result.video_categories.length) {
-                    data.categories = result.video_categories.map(({ name, id }) => ({
-                        name: escape(name),
-                        id,
-                        type: 'video_categories'
-                    }));
-                }
-
-                if (result.video_shows) {
-                    data.shows = result.video_shows.map(({ name, id }) => ({
-                        name: escape(name),
-                        id,
-                        type: 'video_show'
-                    }));
-                }
-                return data;
-            })
-            .then(data => {
-                return ATV.Ajax.get(prepareUrl(`${getPath('video/get-saved-time/')}&video_id=${data.id}`))
-                    .then(response => {
-                        data.savedTime = Math.floor(ATV._.get(response, 'response.savedTime', -1));
-                        if (data.savedTime < 0) {
-                            data.savedTime = 0;
-                            data.hideRestart = true;
-                        }
-                        return data;
-                    });
-            })
-            .then(data => {
                 if (data.categories || data.shows) {
                     const promises = [].concat(data.shows, data.categories)
                         .filter(item => !!item)
-                        .map(generateShowPromise);
+                        .map(generateShowPromise(savedTimes));
                     Promise.all(promises)
                         .then(res => {
                             data.shelves = res;
@@ -181,17 +181,24 @@ const Page = ATV.Page.create({
     }
 });
 
-const generateShowPromise = ({ name, id, type }) => {
+const generateShowPromise = (savedTimes = {}) => ({ name, id, type }) => {
     const url = prepareUrl(`${getPath('videos')}&filter=${type}:${id}&limit=10`);
 
     return ATV.Ajax.get(url)
         .then((response) => {
             let videos = ATV._.get(response, 'response.results', []).map(video => {
-                return {
-                    url: video.api_detail_url,
+                const item = {
+                    id: video.id,
+                    detailUrl: video.api_detail_url,
                     name: video.name,
                     image: ATV._.get(video, 'image.screen_url')
                 };
+
+                if (savedTimes[item.id]) {
+                    item.progress = ATV._.round(savedTimes[item.id].savedTime / video.length_seconds, 2);
+                }
+
+                return item;
             });
 
             return { name, videos };
